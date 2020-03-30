93b6d223dbde091d9fb044451a3af109ef37ba74
MrWhite <hugoblanc.blend@gmail.com>
2020-03-30 21:09:31 +0200

    working versionn: 20%

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
+var util = require('util')
 const datas = [
-    { values: [1, 0, 1, 0, 1, 0, 0, 0, 0], target: 1 },
-    { values: [0, 0, 0, 0, 0, 1, 1, 1, 1], target: 0 },
-    { values: [0, 1, 0, 1, 1, 0, 0, 0, 0], target: 1 },
-    { values: [0, 0, 0, 0, 1, 1, 1, 1, 1], target: 0 },
-    { values: [1, 0, 1, 1, 1, 0, 0, 0, 0], target: 1 },
-    { values: [0, 0, 0, 0, 1, 1, 0, 1, 1], target: 0 },
+    { values: [1], target: 1 },
+    { values: [0], target: 0 },
+    // { values: [0, 1, 0, 1, 1, 0, 0, 0, 0], target: 1 },
+    // { values: [0, 0, 0, 0, 1, 1, 1, 1, 1], target: 0 },
+    // { values: [1, 0, 1, 1, 1, 0, 0, 0, 0], target: 1 },
+    // { values: [0, 0, 0, 0, 1, 1, 0, 1, 1], target: 0 },
 ];
-const velocity = 0.000001;
+const velocity = 0.1;
 
 
 let globActivateCounter = 0;
+let globRecursiveCounter = 0;
+let globCostCalcul = 0;
+
+let localRecursive = 0;
 
 function sigmoid(x) {
     return 1 / (1 + Math.pow(Math.E, -x))
@@ -19,20 +24,26 @@ function derivedSigmoid(x) {
 }
 
 class Neuron {
-    constructor(nbParents) {
-        // Init weights
-        this.weights = [];
+    constructor(nbParents, parentLayer = []) {
+        // Init synapses
+        this.synapses = [];
         this.sum = 0;
         this.activation = 0;
+        this.childSynapses = [];
         for (let i = 0; i < nbParents; i++) {
-            this.weights.push((Math.random() - 0.5) * 2);
+            const parentNeuron = parentLayer[i] ? parentLayer[i] : null;
+            this.synapses.push({ weight: i===0 ? 1 : (Math.random() - 1) * 2, parentNeuron });
+            if (parentNeuron) {
+                parentNeuron.childSynapses.push(this);
+            }
         }
     }
 
     activate(inputs) {
         let total = 0;
         for (let i = 0; i < inputs.length; i++) {
-            const wN = inputs[i] * this.weights[i];
+            const weight = this.synapses[i] ? this.synapses[i].weight : 1;
+            const wN = inputs[i] * weight;
             total += wN;
         }
 
@@ -50,21 +61,29 @@ class Network {
             throw new Error('You motherfucker are forgeting some layers')
         }
         this.layers = [];
-        for (let i = 0; i < nb.length - 1; i++) {
-            const prevLayer = nb[i];
+        for (let i = 0; i < nb.length; i++) {
+            const prevLayer = nb[(i > 0 ? i - 1 : 0)];
             const nextLayer = nb[i];
+            // console.log(prevLayer,' and ', nextLayer);
+
             this.layers.push(this.createLayer(prevLayer, nextLayer))
 
         }
-        this.layers.push(this.createLayer(nb[nb.length - 1], 1))
+        this.addFinalLayer(this.layers[this.layers.length - 1]);
+
     }
 
     createLayer(nbParent, nbNeurons) {
         let layer = [];
         for (let i = 0; i < nbNeurons; i++) {
-            layer.push(new Neuron(nbParent));
+            const prevLayer = this.layers.length > 0 ? this.layers[this.layers.length - 1] : undefined;
+            layer.push(new Neuron(nbParent, prevLayer));
         }
         return layer;
+    }
+
+    addFinalLayer(prevLayer) {
+        this.layers.push(this.createLayer(prevLayer.length, 1))
     }
 
     activate(inputs, layerIndex = 0) {
@@ -80,41 +99,71 @@ class Network {
         return this.activate(accFuncResults, ++layerIndex);
     }
 
-    backPropagation(target) {
+
+    backPropagationBis(target) {
         const lastLayer = this.layers[this.layers.length - 1];
-        for (const r of lastLayer) {
-            const prevLayer = this.layers[this.layers.length - 2];
-            for (let i = 0; i < prevLayer.length; i++) {
-                const prevNeuron = prevLayer[i];
-                let derivedPrevWeightCost = this.derivedCost(r, prevNeuron, target);
-                r.weights[i] -= derivedPrevWeightCost * velocity;
+
+        for (const finalNeuron of lastLayer) {
+            // console.log(finalNeuron);
+            const dC = 2 * (finalNeuron.activation - target);
+            for (const synapse of finalNeuron.synapses) {
+                const dCostPZ = this.derivedCostPartialZ(dC, synapse);
+                localRecursive = 0;
+                // console.log(++localRecursive);
+
+                this.recPropagation(dC, finalNeuron, dCostPZ, synapse);
+                localRecursive = 0;
             }
         }
     }
 
-    derivedCost(result, prevNeuron, target) {
-        const dC = 2 * (result.activation - target);
-        const dS = derivedSigmoid(prevNeuron.sum);
-        const prevActivation = prevNeuron.activation;
-        return dC * dS * prevActivation;
+
+    recPropagation(dC, finalNeuron, dCostPZ, ...synapses) {
+        globRecursiveCounter++;
+        const youngestSynapse = synapses[synapses.length - 1];
+
+
+        if (youngestSynapse.parentNeuron == null) return;
+        const parentNeuron = youngestSynapse.parentNeuron;
+        let oldWeight = youngestSynapse.weight;
+        youngestSynapse.weight = youngestSynapse.weight + (dCostPZ * parentNeuron.activation) * (-velocity);
+        // console.log(`Chaine size: ${synapses.length} last${oldWeight} new ${youngestSynapse.weight}  diff ${youngestSynapse.weight - oldWeight}`);
+
+        if (parentNeuron.synapses == null) return;
+        for (let i = 0; i < parentNeuron.synapses.length; i++) {
+            const pSynapse = parentNeuron.synapses[i];
+            if (pSynapse.parentNeuron !== null) {
+                dCostPZ = this.derivedCostPartialZ(dC, pSynapse) * youngestSynapse.weight * dCostPZ;
+                this.recPropagation(dC, finalNeuron, dCostPZ, ...synapses, pSynapse)
+            }
+        }
     }
 
-
+    derivedCostPartialZ(dC, synapse) {
+        globCostCalcul++;
+        const dS = derivedSigmoid(synapse.parentNeuron.sum);
+        // const prevActivation = synapse.parentNeuron.activation;
+        return dC * dS;
+    }
 
 }
 
 
-const network = new Network(datas[0].values.length, 9);
+const network = new Network(datas[0].values.length, 2, 5, 10);
+// console.log(network);
 
 let result;
-
-for (let i = 0; i < 500000; i++) {
+for (let i = 0; i < 30000; i++) {
     result = network.activate(datas[i % datas.length].values);
-    console.log(result, 'should be => ' + datas[i % datas.length].target);
-    network.backPropagation(datas[i % datas.length].target)
+    // console.log(util.inspect(network, {depth: 10}));
+    console.log(result, 'Predict => ', (result > 0.5) ? 1 : 0,  ' should be => ' + datas[i % datas.length].target);
+    network.backPropagationBis(datas[i % datas.length].target)
+    // console.log(util.inspect(network, {depth: 10}));
 }
 
 
 
 
 console.log("Neuron activation" + globActivateCounter);
+console.log("BackProp recursive call" + globRecursiveCounter);
+console.log("Cost derived calculation" + globCostCalcul);
